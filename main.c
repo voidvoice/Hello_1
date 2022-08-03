d5892d896120015c985678cfa47275e9eaa45ac2
voidvoice <rshanshrestha@gmail.com>
2022-08-03 19:08:37 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("Menu\n\t");
+    printf("Tea\n\t");
+    printf("Coffee\n\t");
+    printf("Milk\n\t");
+    printf("\t..Thank You!..\n");
+    return 0;
+}
